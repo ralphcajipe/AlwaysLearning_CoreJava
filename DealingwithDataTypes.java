6a855798b1c52f129174fac56995ecf74d809d9c
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-04-11 19:26:24 +0800

    Dealing with Data Types and Operators

---
+++ DealingwithDataTypes.java
@@ -0,0 +1,11 @@
+public class DealingwithDataTypes {
+    public static void main(String[] args){
+        double x = 16;
+        x%=4; // Get remainder from x = 16 % 4 which is 0
+        x*=2; // x = 0*2 is 0
+        System.out.println("Value of x: " +x);
+    }
+    
+}
+
+       
