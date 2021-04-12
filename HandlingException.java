b8164155ad450808bac5a700ecac16fffc8b2787
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-04-13 01:46:32 +0800

    Handling Exception

--- HandlingException.java
+++ HandlingException.java
@@ -3,16 +3,16 @@ class HandlingException {
         try {
             // Throws Arithmetic Exception
             System.out.println(10/2); 
-            System.out.println("1. This will print if no error.");          
+            System.out.println("1 - This will print if no error.");          
         }
         catch (ArithmeticException e) { 
-            System.out.println("2. Arithmetic Exception catched it.");
+            System.out.println("2 - Arithmetic Exception catched it.");
         } 
         catch (Exception e) {
-            System.out.println("3. Exception catched it.");
+            System.out.println("3 - Exception catched it.");
         } 
         finally {
-            System.out.println("4. Finally is always executed.");
+            System.out.println("4 - Finally is always executed.");
         }
     }
 }
