0162adbb8ff9e2da7d14cd5f4cc3604a03f21b79
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-04-13 15:03:04 +0800

    Initiate versus Instantiate

---
+++ InitiateVersusInstantiate.java
@@ -0,0 +1,16 @@
+public class InitiateVersusInstantiate {
+    /**
+     * You have to INITIATE a class to invoke method:
+     * Classname.methodName();
+     * 
+     * Example: ClassTest.methodTest();
+     * 
+     * 
+     * You have to INSTANTIATE a class OBJECT to invoke method:
+     * Classname objectName = new Classname();
+     * objectName.methodName();
+     * 
+     * Example: ClassTest objectTest = new ClassTest
+     * objectTest.methodTest();
+     */
+}
