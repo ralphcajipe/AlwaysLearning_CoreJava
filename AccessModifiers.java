721b57752a30f0e0d08a9cdbe21d424c4dd26577
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-04-13 15:03:29 +0800

    Access Modifiers

---
+++ AccessModifiers.java
@@ -0,0 +1,11 @@
+public class AccessModifiers {
+    /**
+     * public - accessible anywhere. No restriction at all.
+     * 
+     * protected - accessible only in same package or any package as long as there is direct inheritance
+     * 
+     * default - accessible only in the same package. No keyword means default.
+     * 
+     * private - accessible only inside a class where it is declared.
+     */
+}
